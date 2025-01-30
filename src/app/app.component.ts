d04824b6eb533034d10dc615ab9ea25a2cf4260d
Tom Mendy <tom.mendy@epitech.eu>
2025-01-30 19:02:16 +0100

    [ADD] implement RepositoryComponent to fetch and display repository content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RepositoryComponent } from './repository/repository.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RepositoryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  constructor() {
+    console.log('AppComponent created');
+  }
+
+  ngOnInit() {
+    console.log('AppComponent initialized');
+  }
+
+  ngOnDestroy() {
+    console.log('AppComponent destroyed');
+  }
 }
