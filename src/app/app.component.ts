c6fe614a6dbf1e0640f931d46f7e76998bc6c4ab
Tom Mendy <tom.mendy@epitech.eu>
2025-01-31 13:56:18 +0100

    feat: add GitHub component to display pinned repositories and update app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { RepositoryComponent } from './repository/repository.component';
+import { GithubComponent } from './github/github.component';
 import { CVComponent } from './cv/cv.component';
+import { DividerModule } from 'primeng/divider';
 // import { PrimeNG } from 'primeng/config';
 // import Aura from '@primeng/themes/aura';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CVComponent],
+  imports: [RouterOutlet, CVComponent, DividerModule, GithubComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  name = 'Tom Mendy';
+  email = 'tom.mendy@epitech.eu';
 }
---
+++ src/app/github/github.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
+export interface Root {
+  data: Data;
+}
+
+export interface Data {
+  user: User;
+}
+
+export interface User {
+  pinnedItems: PinnedItems;
+}
+
+export interface PinnedItems {
+  nodes: Node[];
+}
+
+export interface Node {
+  name: string;
+}
+
+@Component({
+  selector: 'app-github',
+  imports: [CommonModule],
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css'],
+})
+export class GithubComponent implements OnInit {
+  pinnedItems!: PinnedItems;
+  errorMessage = '';
+
+  private readonly GITHUB_GRAPHQL_URL = 'https://api.github.com/graphql';
+  private readonly GITHUB_TOKEN = environment.GITHUB_TOKEN;
+  private readonly OWNER = 'Tom-Mendy';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchPinnedRepos();
+  }
+
+  private fetchPinnedRepos(): void {
+    const query = `
+      query {
+        user(login: "${this.OWNER}") {
+          pinnedItems(first: 6, types: REPOSITORY) {
+            nodes {
+              ... on Repository {
+                name
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const headers = new HttpHeaders({
+      Authorization: `bearer ${this.GITHUB_TOKEN}`,
+      'Content-Type': 'application/json',
+    });
+
+    this.http.post<Root>(this.GITHUB_GRAPHQL_URL, { query }, { headers }).subscribe({
+      next: (response) => {
+        this.errorMessage = '';
+        this.pinnedItems = response.data.user.pinnedItems;
+      },
+      error: (error) => {
+        this.errorMessage = error;
+        this.pinnedItems = { nodes: [] };
+      },
+    });
+  }
+}
