b4ff7a4c1c3d05f0bde895f59d816fa72d017cfb
Tom Mendy <tom.mendy@epitech.eu>
2025-01-31 10:49:51 +0100

    feat: update CV display layout and enhance dialog styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { RepositoryComponent } from './repository/repository.component';
 import { CVComponent } from './cv/cv.component';
+// import { PrimeNG } from 'primeng/config';
+// import Aura from '@primeng/themes/aura';
 
 @Component({
   selector: 'app-root',
