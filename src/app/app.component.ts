58737622604ee68e2ef4e57d8b0024b911df935a
Tom Mendy <tom.mendy@epitech.eu>
2025-02-05 12:46:06 +0100

    feat: remove GitHub component from app layout to streamline imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GithubComponent } from './github/github.component';
 import { ContactComponent } from './contact/contact.component';
 import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactComponent, DividerModule, GithubComponent],
+  imports: [RouterOutlet, ContactComponent, DividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
