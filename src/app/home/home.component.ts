79d819db6ba0c355e65f5605f386e42f1d0260b8
Tom Mendy <tom.mendy@epitech.eu>
2025-02-09 15:29:25 +0100

    feat: add avatar component to home page for enhanced user profile display

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-home',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, AvatarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
